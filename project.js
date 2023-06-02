172908e36e4faccbf4fb4d9885b5f0b385f6734f
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-06-01 19:25:46 -0500

    Created deposit, bet, and line functions; Wrote function to spin slot machine

---
+++ project.js
@@ -0,0 +1,94 @@
+// 1. Deposit money
+// 2. Determine number of lines to bet on
+// 3. Collect bet amount
+// 4. Spin slot machine
+// 5. Check if user won
+// 6. Give user winnings
+// 7. Play again?
+
+// function deposit() {
+//     return 1
+// }
+
+const prompt = require("prompt-sync")();
+// Extra () gives access to function that takes in user input
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}
+// multiplier
+const SYMBOL_VALUES ={
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter a number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+          console.log("Invalid number of lines, try again.");
+        } else {
+          return numberOfLines;
+        }
+      }
+}
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the total bet per line: ");
+        const numberBet = parseFloat(bet);
+    
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+          console.log("Invalid bet, try again.");
+        } else {
+          return numberBet;
+        }
+      } 
+};
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        for (let j = 0; j < ROWS; j++) {
+            
+        }
+    }
+};
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
+
+
+
