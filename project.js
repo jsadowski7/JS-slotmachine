87c3c2de3fee6336fb87cb1a652e0bc6e0a4d484
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-06-01 19:44:30 -0500

    Finished setting up reels and spin function

--- project.js
+++ project.js
@@ -77,18 +77,24 @@ const spin = () => {
         }
     }
 
-    const reels = [[], [], []];
+    const reels = [];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
+        const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
-            
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
         }
     }
+    return reels;
 };
 
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 
-
-
-
+// 46:25
